69462ea7d764e9a8a16a6e87280be51b3fff4056
Rahat@1990 <mzorahat@gmail.com>
2022-06-13 14:16:52 -0400

    this is git

---
+++ atmBanking/src/test/java/testCases/TestAmazon.java
@@ -0,0 +1,26 @@
+package testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestAmazon 
+{
+	@Test
+	public void openAmazon()
+	{
+		ChromeOptions options =new ChromeOptions();
+		options.setHeadless(true);
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver(options);
+		driver.get("https://www.amazon.com");
+		System.out.print("The page title is "+driver.getTitle());
+		
+		
+	}
+
+}
