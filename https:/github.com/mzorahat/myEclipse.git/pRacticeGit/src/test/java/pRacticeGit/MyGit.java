c7b724241762e9b0ab9aeedcd1776d01199be0e1
Rahat@1990 <mzorahat@gmail.com>
2022-06-13 14:09:14 -0400

    this is my git

---
+++ https:/github.com/mzorahat/myEclipse.git/pRacticeGit/src/test/java/pRacticeGit/MyGit.java
@@ -0,0 +1,12 @@
+package pRacticeGit;
+
+public class MyGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" This is the my test");
+		System.out.println("My next line");
+
+	}
+
+}
